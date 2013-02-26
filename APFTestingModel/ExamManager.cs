2fb469d99bbd76adcc25a81b7745d8c37cbf7de0
Alan <fu.lai1@tafensw.net.au>
2013-02-26 16:56:58 +1100

    Added Facade and Manager* Classes

---
+++ APFTestingModel/ExamManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APFTestingModel
+{
+    public class ExamManager : Manager
+    {
+        //Get results and generate report
+
+        public Exam FetchExam(Guid examId)
+        {
+            return context.Exams.FirstOrDefault(e => e.Id == examId);
+        }
+    }
+}
