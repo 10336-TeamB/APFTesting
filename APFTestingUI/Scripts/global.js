37330dbc5e2e7195a1149e0154aebf052d59493f
Alan <fu.lai1@tafensw.net.au>
2013-03-03 23:08:41 +1100

    WebUI styling modified to look more like exisiting website

---
+++ APFTestingUI/Scripts/global.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    var headerHeight = $('#header').height();
+    var menuHeight = $('#menu').height();
+    var footerHeight = $('#footer').height();
+    var contentPadding = parseInt($('#content .size-wrapper').css('padding-top')) + parseInt($('#content .size-wrapper').css('padding-bottom'));
+    var totalHeight = headerHeight + menuHeight + footerHeight + contentPadding;
+    var screenHeight = $(document).height();
+    if(totalHeight < screenHeight)
+        $('#content .size-wrapper').height(screenHeight - totalHeight);
+});
+
