5a1bb8ab6ef762516ee533b35ba57793ab25e128
Alan <fu.lai1@tafensw.net.au>
2013-03-17 11:49:08 +1100

    Removed default CSS and began adding some CSS fixes

--- APFTestingUI/Scripts/global.js
+++ APFTestingUI/Scripts/global.js
@@ -1,13 +1,13 @@
-$(function () {
-    var headerHeight = $('#header').height();
-    var menuHeight = $('#menu').height();
-    var footerHeight = $('#footer').height();
-    var contentPadding = parseInt($('#content .size-wrapper').css('padding-top')) + parseInt($('#content .size-wrapper').css('padding-bottom'));
-    var totalHeight = headerHeight + menuHeight + footerHeight + contentPadding;
-    var screenHeight = $(document).height();
-    if (totalHeight < screenHeight)
-        $('#content .size-wrapper').height(screenHeight - totalHeight);
-});
+//$(function () {
+//    var headerHeight = $('#header').height();
+//    var menuHeight = $('#menu').height();
+//    var footerHeight = $('#footer').height();
+//    var contentPadding = parseInt($('#content .size-wrapper').css('padding-top')) + parseInt($('#content .size-wrapper').css('padding-bottom'));
+//    var totalHeight = headerHeight + menuHeight + footerHeight + contentPadding;
+//    var screenHeight = $(document).height();
+//    if (totalHeight < screenHeight)
+//        $('#content .size-wrapper').height(screenHeight - totalHeight);
+//});
 
 $(function () {
     $("#question-examiner-button").click(function () {
