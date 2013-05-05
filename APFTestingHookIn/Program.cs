8ef564761f5f7c8d9f7430a542e735176a3421f6
Alan <fu.lai1@tafensw.net.au>
2013-05-05 21:43:24 +1000

    CRUD for pilot practical template added to facade

--- APFTestingHookIn/Program.cs
+++ APFTestingHookIn/Program.cs
@@ -6,9 +6,12 @@ namespace APFTestingHookIn {
         public static void Main(string[] args)
         {
             Facade _facade = new Facade();
-
-
-            
+            //_facade.CreateAssessmentTaskPilot("Test Title", "Test Detail", 50);
+            Guid id = new Guid("818fe6fc-a007-4058-9551-c0ff189b4a52");
+            //_facade.EditAssessmentTaskPilot(id, "Test Title 2", "Test Detail 2", 10);
+            //_facade.DeleteAssessmentTaskPilot(id);
+            //Console.WriteLine(_facade.checkIsReferenced(id));
+            //Console.ReadKey();
 
         }
     }
