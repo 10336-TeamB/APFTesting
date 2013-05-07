835005d008fee41a06c2d45505653cc9dd904ef2
Adam-10336 <adam.m.smith@outlook.com>
2013-05-07 17:33:22 +1000

    added initial view for TheoryComponent Index

---
+++ APFTestingUI/Areas/Administration/Models/TheoryFormat/FormatDisplayItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using APFTestingModel;
+
+namespace APFTestingUI.Areas.Administration.Models.TheoryFormat
+{
+    public class FormatDisplayItem
+    {
+        public FormatDisplayItem(ITheoryComponentFormat format)
+        {
+            Id = format.Id;
+            NumberOfQuestions = format.NumberOfQuestions;
+            PassMark = format.PassMark;
+            TimeLimit = format.TimeLimit;
+        }
+
+        public Guid Id { get; set; }
+        public int NumberOfQuestions { get; set; }
+        public int PassMark { get; set; }
+        public bool IsActive { get; set; }
+        public int TimeLimit { get; set; }
+    }
+}
