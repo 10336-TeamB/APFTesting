7cd61109b07ba382c04052b0ef32e1a8e8d2b0bd
Alan <fu.lai1@tafensw.net.au>
2013-05-21 16:53:05 +1000

    Removing ExamId from URL for Exam Controller Actions

--- APFTestingUI/Scripts/summary.js
+++ APFTestingUI/Scripts/summary.js
@@ -1,4 +1,19 @@
 ﻿$(function () {
+    $("#question-examiner-button").click(function () {
+        $("#question-void-button").slideToggle(300);
+    });
+
+    $("#question-void-button").click(function (e) {
+        e.preventDefault();
+        showLightBox("#void-authentication");
+    });
+
+    $("#void-cancel").click(function () {
+        parent.$.fn.colorbox.close();
+    });
+})
+
+$(function () {
     $("dd").hide();
     $("dt").not("a").click(function (event) {
         var $target = $(event.target);
