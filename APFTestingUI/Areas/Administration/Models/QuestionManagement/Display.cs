05ffb49d7a452257b41e064c14efd2d6dcb4bad7
Joshua <joshua.esmero3@tafensw.net.au>
2013-05-23 17:19:55 +1000

    checked question crud textareas to textboxes

---
+++ APFTestingUI/Areas/Administration/Models/QuestionManagement/Display.cs
@@ -0,0 +1,47 @@
+﻿using APFTestingModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace APFTestingUI.Areas.Administration.Models.QuestionManagement
+{
+    public class Display
+    {
+        #region Constructors
+
+        public Display() { }
+
+        public Display(ITheoryQuestion question)
+		{
+            Id = question.Id;
+            ImagePath = question.ImagePath;
+            Description = question.Description;
+            Category = question.Category;
+            Answers = question.Answers.Select(a => new AnswerItem(a)).ToList();
+            
+		}
+
+		#endregion
+        
+        
+        
+        #region Properties
+
+        public Guid Id { get; private set; }
+
+        public string ImagePath { get; private set; }
+
+        public string Description { get; private set; }
+
+        public TheoryQuestionCategory Category { get; private set; }
+
+        public List<AnswerItem> Answers { get; private set; }
+        
+        #endregion
+
+
+    }
+}
