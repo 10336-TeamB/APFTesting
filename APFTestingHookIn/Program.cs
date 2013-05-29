f47353a4528d961c68eb3b31d3354d42d485772c
Pradipna <pradipna.nepal@tafensw.net.au>
2013-05-29 15:06:59 +1000

    Added Binary Tree

--- APFTestingHookIn/Program.cs
+++ APFTestingHookIn/Program.cs
@@ -12,30 +12,84 @@ namespace APFTestingHookIn
     {
         static void Main(string[] args)
         {
-            PDFGenerator pdf = new PDFGenerator();
+            BinaryTree<int> binaryTree = new BinaryTree<int>();
 
-            List<KeyValuePair<string, string>> examDetails = new List<KeyValuePair<string, string>>();
+            Console.WriteLine("Unsorted Data:");
+            Random rand = new Random();
+            for (int i = 0; i < 14; ++i)
+            {
+                int r = rand.Next();
+                binaryTree.insertValue(r);
+                Console.WriteLine(r);
+            }
 
-            examDetails.Add(new KeyValuePair<string, string>("Name", String.Format("{0} {1}", "John", "Carmack")));
-            examDetails.Add(new KeyValuePair<string, string>("D.O.B.", "20/08/1970"));
-            examDetails.Add(new KeyValuePair<string, string>("ARN", "654123"));
-            examDetails.Add(new KeyValuePair<string, string>("Phone Number", "0452199039"));
-            examDetails.Add(new KeyValuePair<string, string>("Mobile Number", "0452199039"));
-            examDetails.Add(new KeyValuePair<string, string>("Address 1", "8/28 Subway Road"));
-            examDetails.Add(new KeyValuePair<string, string>("Address 2", ""));
-            examDetails.Add(new KeyValuePair<string, string>("Suburb", "Rockdale, Sydney"));
-            examDetails.Add(new KeyValuePair<string, string>("State", "NSW"));
-            examDetails.Add(new KeyValuePair<string, string>("Email", "i.am.carmack@gmail.com"));
-            examDetails.Add(new KeyValuePair<string, string>("Pilot License Type", "ATPL"));
-            examDetails.Add(new KeyValuePair<string, string>("Instrument Rating", "Yes"));
-            examDetails.Add(new KeyValuePair<string, string>("Valid BFR", "Yes"));
-            examDetails.Add(new KeyValuePair<string, string>("Pilot Medical Type", "Class One"));
-            examDetails.Add(new KeyValuePair<string, string>("Pilot Medical Expiry Date", "NEVER!!!"));
-            examDetails.Add(new KeyValuePair<string, string>("Score", "100/100(100%) - Pass"));
 
-            MemoryStream memStream = pdf.CreatePDF(examDetails, "123456", 1, 0);
-            FileStream fs = new FileStream("C:/Users/p406/Desktop/test.pdf", FileMode.Truncate, FileAccess.Write);
-            fs.Write(memStream.ToArray(), 0, memStream.ToArray().Count());
+            List<int> sortedList = binaryTree.TraverseInOrder();
+
+            Console.WriteLine("\nSorted Data:");
+            for (int i = 0; i < 14; ++i)
+            {
+                Console.WriteLine(sortedList[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class MergeSort<T> where T : IComparable
+    {
+        private List<T> merge(List<T> left, List<T> right)
+        {
+            List<T> result = new List<T>();
+
+            int leftCounter = 0, rightCounter = 0;
+            while (leftCounter < left.Count() || rightCounter < right.Count())
+            {
+                if (leftCounter < left.Count() && rightCounter < right.Count())
+                {
+                    if (left[leftCounter].CompareTo(right[rightCounter]) <= 0)
+                    {
+                        result.Add(left[leftCounter]);
+                        ++leftCounter;
+                    }
+                    else
+                    {
+                        result.Add(right[rightCounter]);
+                        ++rightCounter;
+                    }
+                }
+                else if (leftCounter < left.Count())
+                {
+                    result.Add(left[leftCounter]);
+                    ++leftCounter;
+                }
+                else
+                {
+                    result.Add(right[rightCounter]);
+                    ++rightCounter;
+                }
+            }
+
+            return result;
+        }
+
+        public List<T> mergeSort(List<T> data)
+        {
+            if (data.Count() <= 1)
+            {
+                return data;
+            }
+
+            int mid = data.Count() / 2;
+            List<T> left, right;
+
+            left = data.GetRange(0, mid);
+            right = data.GetRange(mid, data.Count() - mid);
+
+            left = mergeSort(left);
+            right = mergeSort(right);
+
+            return merge(left, right);
         }
     }
 }
---
+++ APFTestingModel/Helpers/MergeSort.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APFTestingModel
+{
+    public class MergeSort<T> where T : IComparable
+    {
+        private List<T> merge(List<T> left, List<T> right)
+        {
+            List<T> result = new List<T>();
+
+            int leftCounter = 0, rightCounter = 0;
+            while (leftCounter < left.Count() || rightCounter < right.Count())
+            {
+                if (leftCounter < left.Count() && rightCounter < right.Count())
+                {
+                    if (left[leftCounter].CompareTo(right[rightCounter]) <= 0)
+                    {
+                        result.Add(left[leftCounter]);
+                        ++leftCounter;
+                    }
+                    else
+                    {
+                        result.Add(right[rightCounter]);
+                        ++rightCounter;
+                    }
+                }
+                else if (leftCounter < left.Count())
+                {
+                    result.Add(left[leftCounter]);
+                    ++leftCounter;
+                }
+                else
+                {
+                    result.Add(right[rightCounter]);
+                    ++rightCounter;
+                }
+            }
+
+            return result;
+        }
+
+        public List<T> mergeSort(List<T> data)
+        {
+            if (data.Count() <= 1)
+            {
+                return data;
+            }
+
+            int mid = data.Count() / 2;
+            List<T> left, right;
+
+            left = data.GetRange(0, mid);
+            right = data.GetRange(mid, data.Count() - mid);
+
+            left = mergeSort(left);
+            right = mergeSort(right);
+
+            return merge(left, right);
+        }
+    }
+}
