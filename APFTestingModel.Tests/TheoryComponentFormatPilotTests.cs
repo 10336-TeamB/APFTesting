6a026f2338d939599f60fda99ec61b8f31059198
Joshua <joshua.esmero3@tafensw.net.au>
2013-06-10 16:55:21 +1000

    did more unit tests

--- APFTestingModel.Tests/TheoryComponentFormatPilotTests.cs
+++ APFTestingModel.Tests/TheoryComponentFormatPilotTests.cs
@@ -20,14 +20,91 @@ namespace APFTestingModel.Tests
             var format = new TheoryComponentFormatPilot(expectedQuestionCount, expectedPassMark, expectedTimeLimit);
 
             // Act
-            var actualQuestionCount = format.NumberOfQuestions;
-            var actualPassMark = format.PassMark;
-            var actualTimeLimit = format.TimeLimit;
+			var actualQuestionCount = format.NumberOfQuestions;
+			var actualPassMark = format.PassMark;
+			var actualTimeLimit = format.TimeLimit;
             
             // Assert
-            Assert.AreEqual(expectedQuestionCount, actualQuestionCount);
-            Assert.AreEqual(expectedPassMark, actualPassMark);
-            Assert.AreEqual(expectedTimeLimit, actualTimeLimit);
+            Assert.AreEqual(expectedQuestionCount, actualQuestionCount,"QuestionCount property did not equal expected value.");
+            Assert.AreEqual(expectedPassMark, actualPassMark, "PassMark property did not equal expected value.");
+            Assert.AreEqual(expectedTimeLimit, actualTimeLimit, "TimeLimit property did not equal expected value.");
         }
+
+		[TestMethod]
+		public void Activate_SetIsActiveToTrue()
+		{
+			//=== ASSEMBLE ===
+			var format = new TheoryComponentFormatPilot();
+
+			//=== ACT ===
+			format.Activate();
+
+			//=== ASSERT ===
+			var actual = format.IsActive;
+			Assert.IsTrue(actual, "The value of IsTrue property is not the expected value of True.");
+
+		}
+
+		[TestMethod]
+		public void Deactivate_SetIsActiveToFalse()
+		{
+			//=== ASSEMBLE ===
+			var format = new TheoryComponentFormatPilot();
+
+			//=== ACT ===
+			format.Deactivate();
+
+			//=== ASSERT ===
+			var actual = format.IsActive;
+			Assert.IsFalse(actual, "The value of IsTrue property is not the expected value of False.");
+		}
+
+		[TestMethod]
+		public void AllowEditOrDelete_WhenConditionTrue_ReturnTrue()
+		{
+			//=== ASSEMBLE ===
+			var format = new TheoryComponentFormatPilot();
+
+			//=== ACT ===
+			format.Deactivate();
+
+			//=== ASSERT ===
+			var actual = format.AllowEditOrDelete;
+			Assert.IsTrue(actual);
+		}
+
+		[TestMethod]
+		public void AllowEditOrDelete_WhenPracticalComponentPilotsCountGreaterThanZero_ReturnFalse()
+		{
+			//=== ASSEMBLE ===
+			var format = new TheoryComponentFormatPilot();
+			format.TheoryComponents = new List<TheoryComponent>()
+			{
+				new TheoryComponentPilot()
+			};
+
+			//=== ACT ===
+			format.Deactivate();
+
+			//=== ASSERT ===
+			var actual = format.AllowEditOrDelete;
+			Assert.IsFalse(actual);
+
+		}
+
+		[TestMethod]
+		public void AllowEditOrDelete_WhenIsActiveIsTrue_ReturnFalse()
+		{
+			//=== ASSEMBLE ===
+			var format = new TheoryComponentFormatPilot();
+
+			//=== ACT ===
+			format.Activate();
+
+			//=== ASSERT ===
+			var actual = format.AllowEditOrDelete;
+			Assert.IsFalse(actual);
+		}
+
     }
 }
